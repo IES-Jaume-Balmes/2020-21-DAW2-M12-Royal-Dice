408cf112edba16b71dca82d94fd8d5743c03f332
polbalada <pbalada19@jaumebalmes.net>
2021-05-04 16:40:02 +0200

    login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,61 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="ltr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
     <title>Login</title>
-    <style>
-        body {
-            background-color: #171717;
-            color: white;
-            text-align: center;
-        }
-        
-        input {
-            color: #917839;
-        }
-        
-         ::placeholder {
-            color: #917839;
-            opacity: 0.5;
-        }
-        
-        a {
-            text-decoration: none;
-            color: white;
-            background-color: #917839;
-        }
-        
-        section {
-            background-color: #917839;
-            width: 20%;
-            padding-bottom: 20px;
-            margin: auto;
-            border-radius: 20px;
-        }
-        .logo {
-            width: auto;
-            align-items: center
-        }
-    </style>
+    <link rel="stylesheet" href="{{ asset('../resources/css/style.css')}}">
 </head>
 
 <body>
-    <div class="logo">
-        <h1>Royal <img src="{{ asset('img/logo.png')}}"> Dice</h1>
-    </div>
-    
-    <section>
-        <h3>Iniciar sesión</h3>
-        <input type="text" placeholder="Usuario">
-        <br><br>
-        <input type="password" name="" id="" placeholder="Constraseña">
-        <!-- <hr width="10px"> O <hr width="10px">      -->
-    </section>
-    <br><br>
-    <a href="">Registrarse</a>
+    <img src="{{ asset('img/logo.png')}}" id="logo">
+
+    <form class="box" action="index.html" method="post">
+        <h2>Login</h2>
+        <input type="text" name="" placeholder="Username">
+        <input type="password" name="" placeholder="Password">
+        <input type="submit" name="" value="Login">
+        <a href="">Registrarse</a>
+    </form>
+
+
+
+
 </body>
 
 </html>
