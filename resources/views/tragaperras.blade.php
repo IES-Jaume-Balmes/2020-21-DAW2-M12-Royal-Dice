5a73f15bddb095f8f9c1fac8c091428e56ff8114
franchavezromero <fchavez19@jaumebalmes.net>
2021-05-18 20:34:30 +0200

    Update tragaperras.blade.php

--- resources/views/tragaperras.blade.php
+++ resources/views/tragaperras.blade.php
@@ -6,12 +6,20 @@
 <body>
     @include('layouts/header')
     <div style="text-align: center" class="juego" id="juego">
-        <!-- <h1 style="color: white">Slot Machine !</h1>
-        <button>Pulsa para jugar</button><br>
-        <img src="{{ asset('img/tragaperras/tragagif0.gif') }}" alt="">
-        <img src="{{ asset('img/tragaperras/tragagif1.gif') }}" alt="">
-        <img src="{{ asset('img/tragaperras/tragagif2.gif') }}" alt="">
-        <p>El resultado es: </p> -->
+        
+    </div>
+    <div style="color: white" class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header" id="headingOne">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              Accordion Item #1
+            </button>
+          </h2>
+          <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+        </div>
     </div>
 </body>
 <script src="{{ asset('js/tragaperras.js') }}"></script>
